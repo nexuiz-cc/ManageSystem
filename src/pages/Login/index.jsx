fd097b1339a91de4cc23e247ac627b8c0d67b6aa
nexuiz-cc <41991563+nexuiz-cc@users.noreply.github.com>
2023-07-13 04:12:32 +0900

    fgnfgn

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Input, Button, Checkbox } from "antd";
+import { Form, Button, Checkbox } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import "./index.scss";
 import { useNavigate } from "react-router-dom";
@@ -15,45 +15,35 @@ const Login = (props) => {
     }
   };
   const register = () => {
-    document.getElementById("register").style.display = "flex";
-    document.getElementsByClassName("box")[0].style.height = "600px";
-    document.getElementsByClassName("warpper")[0].style.height = "590px";
+    document.getElementById("register").style.display = "block";
+    document.getElementsByClassName("box")[0].style.height = "700px";
+    document.getElementsByClassName("warpper")[0].style.height = "690px";
   };
+
+  const onregister = ()=>{
+
+  }
 
   return (
     <div className="box">
       <div className="warpper">
         <div className="warpper2">
-          <Form
-            name="normal_login"
-            className="login-form"
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={onFinish}
-          >
-            <Form.Item
-              name="username"   rules={[{   required: true,   message: "Please input your Username!", }, ]}
-            >
+          <Form  name="normal_login"  className="login-form" 
+              initialValues={{ remember: true,}} onFinish={onFinish}>
+                <h2>Login</h2>
+            <Form.Item name="username"  rules={[{   required: true,   message: "Please input your Username!", }, ]} >
               <div className="inputbox">
-                <Input prefix={<UserOutlined className="site-form-item-icon" />}  />
+                <input  type='text'  prefix={<UserOutlined className="site-form-item-icon" />} />
                 <span>Username</span>
+                <i></i>
               </div>
             </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your Password!",
-                },
-              ]}
-            >
-              <Input
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Password"
-              />
+            <Form.Item name="password" rules={[{ required: true, message: "Please input your Password!",}, ]} >   
+              <div className="inputbox">
+                <input  prefix={<LockOutlined className="site-form-item-icon" />} type="password" />
+                <span>Password</span>
+                <i></i>
+              </div> 
             </Form.Item>
             <Form.Item>
               <Form.Item name="remember" valuePropName="checked" noStyle>
@@ -79,41 +69,23 @@ const Login = (props) => {
               </div>
             </Form.Item>
           </Form>
-          <Form className="u--slideDown" id="register">
-            <Form.Item
-              name="username"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your Username!",
-                },
-              ]}
-            >
-              <Input
-                prefix={<UserOutlined className="site-form-item-icon" />}
-                placeholder="Username"
-                className="reguser"
-              />
+          <Form className="u--slideDown register" id="register" name="normal_register"  initialValues={{ remember: true,}} onFinish={onregister}>
+            <Form.Item name="reguser"   rules={[{ required: true, message: "Please input your Username!",  },]}>
+              <div className="inputbox">
+                <input   type="text"  className="reguser"/>
+                <span>Username</span>
+                <i></i>
+              </div> 
             </Form.Item>
-            <Form.Item
-              id="errmsgPassword"
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your Password!",
-                },
-              ]}
-            >
-              <Input
-                prefix={<LockOutlined className="site-form-item-icon" />}
-                type="password"
-                placeholder="Password"
-                className="regpass"
-              />
+            <Form.Item  name="regpass" rules={[{ required: true, message: "Please input your Password!",}, ]} >
+              <div className="inputbox">
+                <input  type="password"  className="regpass"/>
+                <span>Password</span>
+                <i></i>
+              </div> 
             </Form.Item>
             <Form.Item>
-              <Button type="primary" htmlType="submit" className="registerbtn">
+              <Button type="primary" className="registerbtn">
                 register
               </Button>
             </Form.Item>
